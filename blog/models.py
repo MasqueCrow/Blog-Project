0fc6a6071518ae4a529396ad132fa5398251796f
Tchea JiaWei <tcheajiawei@Hotmail.com>
2020-05-22 19:14:17 +0800

    Add comments

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+#To add current time to posts based on UTC in settings.py
 from django.utils import timezone
 from django.urls import reverse
 
@@ -7,7 +8,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User',on_delete = models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    create_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True,null=True)
 
     def publish(self):
@@ -15,9 +16,20 @@ class Post(models.Model):
         self.save()
 
     def approve_comments(self):
+        '''
+        Any one can comment on a post, but all of them not gonna get approved,
+        This method filters out unapproved(approved_comment = False) comments from Comment class,
+        So that only approved(approved_comment = True) comments save to the model 'Post'
+        '''
         return self.comments.filter(approved_comment=True)
 
     def get_absolute_url(self):
+        '''
+        After Someone creates a post, the user need to be taken to the posts
+        detailed view page('post_detail'),
+        Detailed view always needs the primary key(pk) to display/identify the post
+        'self.pk' represents return to the detailed view of same post after creating
+        '''
         return reverse("post_detail",kwargs={'pk':self.pk})
 
     def __str__(self):
@@ -31,10 +43,17 @@ class Comment(models.Model):
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
+        '''
+        To approve a comment and save to db
+        '''
         self.approved_comment = True
         self.save()
 
     def get_absolute_url(self):
+        '''
+        return to the list view(ie the homepage) after commenting on
+        a post, the comments needs approval to display
+        '''
         return reverse("post_list")
 
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -16,37 +16,67 @@ class AboutView(TemplateView):
 class PostListView(ListView):
     model = Post
 
-    #Pythonic way of writing sql query (ORM)
     def get_queryset(self):
-        #Grab the posts where published date is less than or equal to current timezone
-        #order by published_date
+        """
+        Using Django ORM(Object Relational Mapping) with DB, this querysets allows
+        perform SQLqueries in a 'pythonish' way,
+        which just means from model 'Post' grab all 'objects'(ie posts) 'filter' it out based on 'published_date'
+        attribute less than or equal to(lte) the current time(timezone.now()),'order_by' descending order of ("-")
+        published date.
+        lller SQL query
+        'SELECT * FROM Post WHERE published_date <= timezone.now()'
+        """
         return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
 class PostDetailView(DetailView):
+    """
+    Detail view of post, post_detail.html
+    """
     model = Post
 
 class CreatePostView(LoginRequiredMixin,CreateView):
+    """
+    view of formpage to write up the post, similar to 'login_required' decorator CBV's got
+    mixins which inherits to the classes provides the same functionality that a user must be
+    logged in, to add a post
+    """
     login_url = '/login/'
     redirect_field_name = 'blog/post_detail.html'
     form_class = PostForm
     model = Post
 
 class PostUpdateView(LoginRequiredMixin,UpdateView):
+    """
+    The PostUpdateView is same as CreatePostview, but in here there's only
+    updation of the existing one happens
+    """
     login_url = '/login/'
     redirect_field_name = 'blog/post_detail.html'
     form_class = PostForm
     model = Post
 
 class PostDeleteView(LoginRequiredMixin,DeleteView):
+    """
+    This view Deletes the post from the DB,(DeleteView-manages that)
+    if the post is deleted, it takes user back to 'Post list view'
+    """
     model = Post
     success_url = reverse_lazy('post_list')
 
 class DraftListView(LoginRequiredMixin,ListView):
+    """
+    Drafts are unpublished blogs, This view is to show them,
+    this also need login to see
+    """
     login_url = '/login'
     redirect_field_name = 'blog/post_list.html'
     model = Post
 
     def get_queryset(self):
+        """
+        queryset to find which posts are not published
+        by looking up if the 'published_date' == null and order descending by created date
+        """
         return Post.objects.filter(published_date__isnull=True).order_by('create_date')
 
 #######################################
@@ -55,12 +85,24 @@ class DraftListView(LoginRequiredMixin,ListView):
 
 @login_required
 def post_publish(request,pk):
+    """
+    To publish the post, using the method 'publish' in model class 'Post'
+    :param request:
+    :param pk:
+    :return: to the post_detail view of published post
+    """
     post = get_object_or_404(Post,pk=pk)
     post.publish()
     return redirect('post_detail',pk=pk)
 
 @login_required
 def add_comment_to_post(request,pk):
+    """
+    To add a comment when a 'request' with primary key('pk') of post given,
+    :param request:
+    :param pk:
+    :return: the html tag 'form' and the form is created based on logic
+    """
     post = get_object_or_404(Post,pk=pk)
     if request.method == 'POST':
         form = CommentForm(request.POST)
@@ -75,12 +117,26 @@ def add_comment_to_post(request,pk):
 
 @login_required
 def comment_approve(request,pk):
+    """
+    View to approve comment, using the 'approve' method in Comment model,
+    ie, when 'approve' method called it sets the boolean value 'True'
+    :param request:
+    :param pk:
+    :return: to the post_detail view of current post
+    """
     comment = get_object_or_404(Comment,pk=pk)
     comment.approve()
     return redirect('post_detail',pk=comment.post.pk)
 
 @login_required
 def comment_remove(request,pk):
+    """
+    View to Delete a comment, using the 'delete' method in Comment model( native in 'models.Model'),
+    an extra variable required to store the key value of comment(ie key of post) to delete
+    :param request:
+    :param pk:
+    :return: to the post_detail view of current post
+    """
     comment = get_object_or_404(Comment,pk=pk)
     post_pk = comment.post.pk
     comment.delete()
